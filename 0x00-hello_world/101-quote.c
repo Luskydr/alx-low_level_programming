736650f55a86c6ff65a1289788d6015ea786b5fa
luskydr <drluskyent1@gmail.com>
2023-07-14 02:44:50 -0700

    c7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return: 1 (success)
+ * Return:1 (success)
  */
 int main(void)
 { 
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, ch, sizeof(ch));
-return(1);
+return (1);
 }
