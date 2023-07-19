4d126be9aa230c2e3924f64bfcec7682ba2a3649
luskydr <drluskyent1@gmail.com>
2023-07-19 12:16:52 -0700

    101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,39 +3,41 @@
 /**
  * print_times_table - prints the times table of the input,
  * starting with 0.
- * @n: The valu of the times table to be printed.
+ * @n: The value of the times table to be printed.
  */
 
-void print_times_table(int n)
+void print_times_tables(int n)
 
 {
 
-	int num, multi, prod;
+	int num, mult, prod;
 
-	if (n >= 0 && n <= n; num++)
-	{
-	_putchar('0');
-	for (mult = 1; multi <= n; mult++)
-	{
-	_putchar(',');
-	_putchar(',');
-	prod = num * mult;
-	if (prod <= 99)
-	_putchar(' ');
+	if (n >= 0 && n <= 15)
+        {
+        for (num = 0; num <= n; num++)
+        {
+        _putchar('0');
+        for (mult = 1; mult <= n; mult++)
+        {
+        _putchar(',');
+        _putchar(',');
+        prod = num * mult;
+        if (prod <= 99)
+                _putchar(' ');
 	if (prod <= 9)
 	_putchar(' ');
 	if (prod >= 100)
 	{
 	_putchar((prod / 100) + '0');
-	_puthar(((prod / 10)) % 10 + '0');
+	_putchar(((prod / 10)) % 10 + '0');
 	}
 	else if (prod <= 99 && prod >= 10)
-	}
+	{
 	_putchar((prod / 10) + '0');
 	}
 	_putchar((prod % 10) + '0');
-}
+	}
 	_putchar('\n');
 	}
-}
+	}
 }
