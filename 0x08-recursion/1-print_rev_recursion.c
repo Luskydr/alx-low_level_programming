b507a8fd28bca9ff92a6867a8d81bb58343064f3
luskydr <drluskyent1@gmail.com>
2023-08-02 02:45:14 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		return;
+		_print_rev_recursion(s+1);
+		_putchar(*s);
 	}
-
-	s++;
-	_prin_rev_recursion(s);
-	s--;
-	_putchar(*s);
 }
