4bfe42c29d4d44ecc4daaef00eab4a1c5819b799
luskydr <drluskyent1@gmail.com>
2023-08-02 02:49:19 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,15 +2,13 @@
 
 /**
  * _print_rev_recursion - Print a string in reverse
- * @s: the string to reverse
- *
- * Return: Nothing
+ * @s: the string to printed
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
