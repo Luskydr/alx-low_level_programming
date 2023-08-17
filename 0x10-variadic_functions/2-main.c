a6926ec9251a58ee1ff53dee8dc3d2b59ab41be0
luskydr <drluskyent1@gmail.com>
2023-08-17 00:21:46 -0700

    3-print_all.c

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+<S-F12>#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    return (0);
+}
